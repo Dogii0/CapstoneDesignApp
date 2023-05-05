0372c2169a05cd25933635b49459094d7b0e766f
Dolgormaa <d0g11.7.100@gmail.com>
2023-05-05 19:23:42 +0900

    ColorBar and AlphaBar

--- app/src/main/java/kr/ac/dankook/mobile/capstonedesignapp/MainActivity.kt
+++ app/src/main/java/kr/ac/dankook/mobile/capstonedesignapp/MainActivity.kt
@@ -3,9 +3,13 @@ package kr.ac.dankook.mobile.capstonedesignapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.TextView
+import com.rtugeek.android.colorseekbar.AlphaSeekBar
+import com.rtugeek.android.colorseekbar.ColorSeekBar
+import com.rtugeek.android.colorseekbar.ColorSeekBar.*
+import com.rtugeek.android.colorseekbar.OnColorChangeListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,12 +17,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val settingBtn : View = findViewById<Button>(R.id.Settingbtn)
+        val colorSeekBar: ColorSeekBar = findViewById(R.id.ColorSlide)
+        val alphaSeekBar: AlphaSeekBar = findViewById(R.id.AlphaSlide)
+        val test: View = findViewById(R.id.view)
 
         settingBtn.setOnClickListener {
             val intent = Intent(this, SettingPage::class.java)
             startActivity(intent)
         }
 
+        colorSeekBar.setOnColorChangeListener { progress, color ->
+            Log.i("TAG", "===progress:$progress-color:$color===")
+            test.setBackgroundColor(color)
+        }
+        alphaSeekBar.setOnAlphaChangeListener { progress, alpha ->
+            Log.i("AlphaSeekBarFragment", "===progress:$progress-alpha:$alpha===")
+        }
     }
-
 }
